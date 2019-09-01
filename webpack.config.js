ffc1f9353ca7c80ed41371a3b3eb18e781f688e1
Michael Pylypenko <pylypenkomichael@gmail.com>
2019-09-02 01:44:56 +0300

    Message

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const outputPath = path.resolve(__dirname, './dist')
+
+module.exports = {
+	entry: "./src/index.js",
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				enforce: 'pre',
+				use: 'eslint-loader'
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				options: { presets: ["@babel/env"] }
+			},
+			{
+				test: /\.(scss|css)$/,
+				use: ["style-loader", "css-loader", "sass-loader"]
+			},
+			{
+				test: /\.(gif|png|jpg|jpeg|svg)$/,
+				exclude: /node_modules/,
+				include: path.resolve(__dirname, './src/assets/'),
+				use: 'url-loader?limit=10000&name=assets/[name]-[hash].[ext]'
+			}
+		]
+	},
+	resolve: { extensions: ["*", ".js", ".jsx"] },
+	output: {
+		path: path.resolve(__dirname, "dist/"),
+		publicPath: "/dist/",
+		filename: "bundle.js"
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "public/"),
+		port: 5000,
+		publicPath: "http://localhost:3000/dist/",
+		hotOnly: true,
+		historyApiFallback: true,
+		inline: true
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, './public/index.html'),
+			filename: 'index.html',
+			path: outputPath
+		}),
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	]
+};
